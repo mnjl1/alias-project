ebe00c2e7e7f23e1d535a596adab8efd06964a91
mnjl1 <d.manzhula@gmail.com>
2021-07-20 19:56:09 +0300

    gitignore file added

--- alias/models.py
+++ alias/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Alias(models.Model):
+    alias = models.CharField(max_length=255)
+    target = models.CharField(max_length=24)
+    start = models.DateTimeField()
