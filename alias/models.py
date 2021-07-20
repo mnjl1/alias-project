a78d29d97393cce9f5a6df32cdd08596555d94f0
mnjl1 <d.manzhula@gmail.com>
2021-07-20 20:51:39 +0300

    database populated

--- alias/models.py
+++ alias/models.py
@@ -5,4 +5,7 @@ class Alias(models.Model):
     alias = models.CharField(max_length=255)
     target = models.CharField(max_length=24)
     start = models.DateTimeField()
-    end = models.DateTimeField()
+    end = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.alias
